dbd1ce5e3e45dc77f044d23c00dbe45f25ac8b73
Liam Boland <148321025+teemoisaslave@users.noreply.github.com>
2024-05-07 10:32:51 +0000

    prova errore 2.1

--- config.js
+++ config.js
@@ -2,7 +2,6 @@ const fs = require("fs");
 module.exports = {
   host: "mysql-3be64f8f-itis-f13b.a.aivencloud.com",
   user: "avnadmin",
-  password: "AVNS_DKMQmbVZLAsywRIgNFi",
   database: "chat",
   port: 28516,
   ssl: {
